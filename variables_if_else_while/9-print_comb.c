6fac627c88b9a3355b16fb64928679a44d61e9a3
Hedgebzh <axel.gore35@gmail.com>
2022-06-08 17:14:20 +0200

    9.3

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,13 +10,16 @@
 int main(void)
 {
 int x = '0';
-while (x <= '8')
+while (x <= '9')
 {
 putchar(x);
+if (x <= '8')
+{
 putchar(',');
+}
 putchar(' ');
 x++;
 }
-putchar('9');
+putchar('\n');
 return (0);
 }
