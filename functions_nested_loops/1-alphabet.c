2b8a93d1d7b3489e3e159c67e31f4deee6705a14
Hedgebzh <axel.gore35@gmail.com>
2022-06-09 12:12:13 +0200

    1.0

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+int letter;
+for (letter = 97; letter <= 122; letter++)
+_putchar(letter);
+_putchar('\n');
+}
