9bbbfd9afdc90d12362860f78c1fa41039c0dbc1
Hedgebzh <axel.gore35@gmail.com>
2022-06-09 14:21:26 +0200

    4.0

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _isalpha - check uppercase or lowercase
+*@c: character checked
+*Return: 1 if is a letter lowercase, 0 otherwise
+*/
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
