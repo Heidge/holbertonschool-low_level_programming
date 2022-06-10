f1ffec22d3b6af3a282cbb6f9b72b765eab901e4
Hedgebzh <axel.gore35@gmail.com>
2022-06-10 15:25:08 +0200

    9.3

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,17 +5,22 @@
  */
 void times_table(void)
 {
-	int x, y;
+	int x, y, z;
 
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
-			if (x * y / 10 >= 1)
+			z = x * y;
+			if (z > 10)
 			{
-			_putchar(x * y / 10 + '0');
+			_putchar(z / 10 + '0');
 			}
-			_putchar(x * y % 10 + '0');
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar(z % 10 + '0');
 			if (y <= 8)
 			{
 			_putchar(',');
