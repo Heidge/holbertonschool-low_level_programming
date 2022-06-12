06b54d880bf519a12d720468bc22097a3499fc6a
Hedgebzh <axel.gore35@gmail.com>
2022-06-12 16:21:37 +0200

    10.1

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
- *times_table - hard exercice
+ *add - why easy exercice after hard one ?
+ *@x:number1
+ *@y:number2
+ *Return: result
  */
 int add(int x, int y)
 {
 	int z;
 
 	z = x + y;
-	return(z);
+	return (z);
 }
