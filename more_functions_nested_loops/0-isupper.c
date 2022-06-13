f3acef21ae2336ea1a681a68c624a6a547c47909
Hedgebzh <axel.gore35@gmail.com>
2022-06-13 09:47:18 +0200

    0.0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _islower - check lowercase
+*@c: character checked
+*Return: 1 if lowercase, 0 otherwise
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef HEADER
+#define HEADER
+#include <stdio.h>
+int _isupper(int c);
+#endif /* HEADER */
