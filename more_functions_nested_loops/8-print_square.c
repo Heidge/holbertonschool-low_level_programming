e666c03624443d76d6378628000136670b70b80b
Hedgebzh <axel.gore35@gmail.com>
2022-06-13 15:00:06 +0200

    8.1

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -5,14 +5,18 @@
 */
 void print_square(int size)
 {
-	int x;
+	int x, y;
 
 	if (size > 0)
 	{
+		for (y = 0; y < size; y++)
+		{
 		for (x = 0; x < size; x++)
+		{
 			_putchar('#');
-	}
-
-	_putchar('\n');
+		}
+		_putchar('\n');
+		}
+	};
 }
 
