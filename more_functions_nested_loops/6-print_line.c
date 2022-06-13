e075e30f736d95e2f94acf2aa506066da3a5b6e1
Hedgebzh <axel.gore35@gmail.com>
2022-06-13 11:54:58 +0200

    6.0

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_line - function
+*@n: times print
+*/
+void print_line(int n)
+{
+int x;
+for (x = 0; x == n; x++)
+{
+	_putchar('_');
+	if (n <= 0)
+	_putchar('\n');
+	_putchar('\n');
+}
+}
+
