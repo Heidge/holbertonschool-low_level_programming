3e57bcebb95b94377732cd64e51f46068cbe29e3
Hedgebzh <axel.gore35@gmail.com>
2022-06-13 17:34:47 +0200

    9.4

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,22 +15,23 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (x % 5 == 0)
+		else if (x % 5 == 0 && x != 100)
 		{
 			printf("Buzz ");
-		}
-		else if (x == 100)
-		{
-			printf("Buzz");
 		}
 		else if (x % 3 == 0)
 		{
 			printf("Fizz ");
 		}
+		else if (x % 100 == 0)
+		{
+			printf("Buzz");
+		}
 		else
 		{
 			printf("%d ", x);
 		}
+
 	}
-	return (0);
+		return (0);
 }
