704df41691cc9f05653ee896fbb8bbcda90e04a4
Hedgebzh <axel.gore35@gmail.com>
2022-06-13 10:15:12 +0200

    2.0

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _isdigit - check uppercase
+*@c: character checked
+*Return: 1 if lowercase, 0 otherwise
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,10 @@
 #ifndef HEADER
 #define HEADER
+
 #include <stdio.h>
+
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+
 #endif /* HEADER */
