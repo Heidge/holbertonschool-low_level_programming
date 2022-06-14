94d8108774fca3f8bc59b083254c9fb14d226d1a
Hedgebzh <axel.gore35@gmail.com>
2022-06-14 19:46:43 +0200

    2.1

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
 *_strlen - function
+*@s: string
+*Return: result
 */
 int _strlen(char *s)
 {
 	int count = 0;
 
-	for (count = 0; *s != '\0'; count ++)
+	for (count = 0; *s != '\0'; count++)
 	{
 		s++;
 	}
