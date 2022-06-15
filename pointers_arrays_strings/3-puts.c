e75ff06f46a5e6cf85b5dd6f24b8605adda8a9c1
Hedgebzh <axel.gore35@gmail.com>
2022-06-15 09:50:59 +0200

    3.0

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _puts - function
+*@str: string
+*/
+void _puts(char *str)
+{
+	int count = 0;
+
+	for (count = 0; *str != '\0'; count++)
+	{
+		str++;
+		_putchar(*str);
+	}
+}
