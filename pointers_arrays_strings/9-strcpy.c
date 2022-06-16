bb5de20ea3e68955b670e478173b6ea0a290d7e7
Hedgebzh <axel.gore35@gmail.com>
2022-06-16 16:57:52 +0200

    9.1

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,9 +8,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = *src; *dest != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	return(src);
+	return(dest);
 }
