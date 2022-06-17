51c11f80b4bd30b881afd29cc222182fb1738a42
Hedgebzh <axel.gore35@gmail.com>
2022-06-17 14:07:39 +0200

    2.1

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_strncat - function
+*_strncpy - function
 *@dest: string
 *@src: dest string
 *@n: bytes
@@ -8,18 +8,18 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (src[i] != '\0' && i < n)
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    while (i < n)
-    {
-        dest[i] = '\0';
-        i++;
-    }
-    return (dest);
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
