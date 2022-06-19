fd1ec599fc31b0a313131ab6e79d1b11c8bab90a
Hedgebzh <axel.gore35@gmail.com>
2022-06-19 17:46:30 +0200

    7.0

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _print_chessboard - function
+* print_chessboard - function
 *@a: parameter 1
 */
 void print_chessboard(char (*a)[8])
@@ -9,7 +9,7 @@ void print_chessboard(char (*a)[8])
 
 	for (idx1 = 0; a[idx1][7]; idx1++)
 	{
-		for(idx2 = 0; idx2 < 8; idx2++)
+		for (idx2 = 0; idx2 < 8; idx2++)
 		{
 			_putchar(a[idx1][idx2]);
 		}
