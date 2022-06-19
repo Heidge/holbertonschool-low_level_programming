ca7aa1d8a75b08baebe48bc0521d887f16e605cb
Hedgebzh <axel.gore35@gmail.com>
2022-06-19 17:06:03 +0200

    3.1

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -2,37 +2,39 @@
 /**
 * _strspn - function
 *@s: parameter 1
-*@c: parameter 2
+*@accept: parameter 2
 *Return: result
 */
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, k = 0, m = 0;
 
-for(i=0; s[i]!= '\0'; i++)
-{
-for(j=0; accept[j]!='\0'; j++)
-{
-if(s[i]==accept[j])
-{
-k++;
-break;
-}
-else
-k = 0;
-}
-if(k == 0)
-break;
-for(j=0; accept[j]!='\0'; j++)
-{
-if(s[i]==accept[j])
-{
-m++;
-break;
-}
-}
-}
-return 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0 ; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+			k++;
+			break;
+			}
+			else
+			{
+			k = 0;
+			}
+		}
+		if (k == 0)
+		break;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+			m++;
+			break;
+			}
+		}
+	}
+return (0);
 }
 
 
