58739114cadc290b65620611dd3a07d4dce2a62a
Hedgebzh <axel.gore35@gmail.com>
2022-06-19 16:31:39 +0200

    1.0

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _memcpy - function
+*@dest: parameter 1
+*@src: parameter 2
+*@n: index
+*Return: result
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	if ((*dest != '\0') && (*src != '\0'))
+	{
+		while (n)
+		{
+			*dest++ = *src;
+			--n;
+		}
+	}
+	return (dest);
+}
