b4a5c16b5f4a3a819e5a786a7493e0104989a59f
Hedgebzh <axel.gore35@gmail.com>
2022-06-20 12:04:22 +0200

    4.1

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
-*factorial - function
-*Return: counter
-*@n: int
+*_pow_recursion - function
+*Return: result
+*@x: number
+*@y: power
 */
 int _pow_recursion(int x, int y)
 {
-    if (y == 0)
+	if (y == 0)
 	{
-    return (1);
+		return (1);
 	}
-    if (y < 0)
+	if (y < 0)
 	{
-    return (-1);
+		return (-1);
 	}
-    return (_pow_recursion(x, y - 1) * x);
+	return (_pow_recursion(x, y - 1) * x);
 }
 
 
