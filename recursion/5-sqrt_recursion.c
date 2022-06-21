ed7e166c2e316043c93883d999cb2fd4ed8743f9
Hedgebzh <axel.gore35@gmail.com>
2022-06-21 10:07:12 +0200

    5.0

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -5,6 +5,29 @@
 *@n: number to square root
 *@m: test number
 */
+int _sqrt_result(int x, int y)
+{
+	if (y * y == x)
+	{
+	return (y);
+	}
+	return (_sqrt_result(x, y + 1));
+
+}
 
 
+int _sqrt_recursion(int n)
+{
+	int y;
 
+	y = 0;
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (_sqrt_result(n, y));
+}
