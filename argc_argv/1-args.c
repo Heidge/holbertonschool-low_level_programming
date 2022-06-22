1a81141815523f3f3040bcd76f7948937897fdf2
Hedgebzh <axel.gore35@gmail.com>
2022-06-22 13:30:06 +0200

    1.0

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+/**
+ * main - function
+ *
+ * @argc: number of arg
+ * @argv: valor of arg
+ * Return: always 0
+ */
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i <
+	 argc; i++)
+	{
+		argv[i]++;
+		printf("%d\n", i);
+	}
+	return (0);
+}
