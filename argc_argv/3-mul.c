ea87fbc443135922318f65463b23b5a466b49c51
Hedgebzh <axel.gore35@gmail.com>
2022-06-22 14:36:47 +0200

    3.0

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -6,13 +6,15 @@
  * Return: always 0
  */
 #include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char **argv)
 {
-	int i = 0;
+	int mul;
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i] - 1);
-	}
+	(void)argc;
+	mul = num1 * num2;
+	printf("%d\n", mul);
 	return (0);
 }
