0e5bac679ddff0cfb666146c474b00e49d457a51
Hedgebzh <axel.gore35@gmail.com>
2022-06-22 13:01:49 +0200

    0.0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,16 +1,16 @@
 /**
  * main - function
  *
- * @param argc
- * @param argv
- * @return int
+ * @argc: number of arg
+ * @argv: valor of arg
+ * Return: always 0
  */
 #include <stdio.h>
 int main(int argc, char **argv)
 {
 	int i;
 
-	for (i = 0; i == argc; i++);
+	for (i = 0; i == argc; i++)
 	printf("%s\n", argv[i]);
 	return (0);
 }
