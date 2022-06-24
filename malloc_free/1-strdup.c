1b1bb48dcc48b7bb0c9e0661f384b5d2f213cd70
Hedgebzh <axel.gore35@gmail.com>
2022-06-24 09:12:34 +0200

    1.0

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+/**
+ * create_array - function
+ *
+ * @size: number of arg
+ * @c: value of arg
+ * Return: always 0
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+char *_strdup(char *str)
+{
+	unsigned int i;
+	unsigned int size;
+	char *array;
+
+	size = strlen(str);
+
+	array = malloc(sizeof(char) * size);
+	if (size == 0)
+	return (NULL);
+
+	if (array == NULL)
+	return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = *str;
+	}
+	return (str);
+}
