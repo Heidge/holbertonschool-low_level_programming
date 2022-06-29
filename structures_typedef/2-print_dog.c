3d3f27ebe433a3303ffca1da28a7894b997391ee
Hedgebzh <axel.gore35@gmail.com>
2022-06-29 12:21:14 +0200

    2.0

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,5 +9,8 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	printf("");
+	printf("nothing");
+	if ((*d).name == NULL || (*d).age == 0 || (*d).owner == NULL)
+	printf("(nil)");
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 }
