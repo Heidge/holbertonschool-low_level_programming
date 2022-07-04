b6cfccb1e96aab28c6ca1dc2fd16789036d48548
Hedgebzh <axel.gore35@gmail.com>
2022-07-04 15:15:44 +0200

    1.0

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_numbers - function.
+ * @n: arg
+ * @separator: separator
+ * Return: sum
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list par;
+	unsigned int count;
+
+	va_start(par, n);
+
+	for (count = 1; count <= n; count++)
+	{
+		printf("%d", va_arg(par, int));
+		if (count <= (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(par);
+}
