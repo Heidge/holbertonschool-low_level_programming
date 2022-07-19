6dc6737759f622fcdafda81884c001b01b965c49
Hedgebzh <axel.gore35@gmail.com>
2022-07-19 14:43:15 +0200

    fix desc and header

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -4,7 +4,7 @@
  * clear_bit - Counts the number of bits needed to be
  *             flipped to get from one number to another.
  * @n: The number.
- * @m: The number to flip n to.
+ * @index: The number to flip n to.
  * Return: The necessary number of bits to flip to get from n to m.
  */
 
