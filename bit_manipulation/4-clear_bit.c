b324a9e0d037860d20caed130ee8044f706da2ac
Hedgebzh <axel.gore35@gmail.com>
2022-07-19 14:41:45 +0200

    fix desc and header

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,3 +1,13 @@
+#include "main.h"
+
+/**
+ * flip_bits - Counts the number of bits needed to be
+ *             flipped to get from one number to another.
+ * @n: The number.
+ * @m: The number to flip n to.
+ * Return: The necessary number of bits to flip to get from n to m.
+ */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
