a7540aecd8104c3810acc0e0f29dd2fed0c0e4c7
Hedgebzh <axel.gore35@gmail.com>
2022-07-20 15:08:09 +0200

    first test task 0 file i/o

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,11 +2,25 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t count;
+	ssize_t fd, r, w;
+	char* buffer;
 
-	for (count = 0; count <= letters; count ++)
-	{
-		filename++;
-	}
-	return (letters);
+	if (filename == NULL)
+	return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	return (0);
+
+	fd = open(filename, O_RDONLY);
+	r = read (fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (fd == -1 || r == -1 || w == -1 || w != r)
+	return (0);
+
+	free(buffer);
+	close(fd);
+	return (w);
 }
+
