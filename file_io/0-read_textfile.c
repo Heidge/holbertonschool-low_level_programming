933f43de3a2dc898cc89dd7d6336a2226eca1864
Hedgebzh <axel.gore35@gmail.com>
2022-07-21 14:21:22 +0200

    task 1 & 2 project file i/o

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ * Return: If the function fails or filename is NULL - 0.
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fd, r, w;
-	char* buffer;
+	char *buffer;
 
 	if (filename == NULL)
 	return (0);
@@ -13,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	return (0);
 
 	fd = open(filename, O_RDONLY);
-	r = read (fd, buffer, letters);
+	r = read(fd, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
 	if (fd == -1 || r == -1 || w == -1 || w != r)
