e1ebcc718e522036f8cf4795b539839796fe3401
Hedgebzh <axel.gore35@gmail.com>
2022-08-25 15:22:16 +0200

    linear search fix and O files

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -2,11 +2,11 @@
 
 /**
   * linear_search - Searches for a value in an array
-  * @array: pointer to the first element of the array to search.
-  * @size: number of elements in the array.
-  * @value: value to search for.
+  * @array: pointer to the first element of the array to search
+  * @size: number of elements in the array
+  * @value: value to search for
   *
-  * return: If the value is not present or the array is NULL, -1.
+  * Return: If the value is not present or the array is NULL, -1
   */
 int linear_search(int *array, size_t size, int value)
 {
