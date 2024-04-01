8ef042e733439665f92fe7a524641a50fe78cc56
Axel Goré <axel.gore@outlook.fr>
2024-04-01 17:31:41 +0200

    Push task 1 files

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+size_t list_len(const list_t *h)
+{
+    size_t len =0;
+
+    while (h)
+    {
+        len++;
+        h=h->next;
+    }
+
+    return len;
+}
