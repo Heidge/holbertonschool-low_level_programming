6c7a8f901e26b4feca8a486047c0a9a62bdf4bd4
Axel Goré <axel.gore@outlook.fr>
2024-04-03 14:08:28 +0200

    Push task 3 & 4 files

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -4,25 +4,31 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *node;
+    list_t *node, *temp;
+    int i = 0;
 
     node = malloc(sizeof(node));
-    if (!node)
+    if(!node)
         return NULL;
 
-    node->str=strdup(str);
-    node->len=strlen(str);
+    while(str[i])
+        i++;
+
+    node->str = strdup(str);
+    node->len = i;
     node->next = NULL;
 
-    if(*head == NULL)
+    temp = *head;
+
+    if (!*head)
         *head = node;
     else
     {
-        list_t *temp = *head;
         while(temp->next != NULL)
             temp = temp->next;
         temp->next = node;
     }
+    
 
-    return node;
+    return(*head);        
 }
