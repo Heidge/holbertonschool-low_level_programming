b1a6f849941631d567429ccc447eccf8a7fc5e86
Axel Goré <axel.gore@outlook.fr>
2024-04-03 13:45:03 +0200

    Push task 1 file

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -3,13 +3,12 @@
 
 size_t list_len(const list_t *h)
 {
-    size_t len =0;
+    int count = 0;
 
-    while (h)
+    while(h)
     {
-        len++;
+        count++;
         h=h->next;
-    }
-
-    return len;
+    } 
+    return(count);
 }
