b5ad8a24713315bc04d443b4daa412a928846910
Heidge <axel.gore@outlook.fr>
2024-05-13 17:00:30 +0200

    Push all task template and readme

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Printf last digit of a random number and say if >5, =0 or less than 6
+* Return: always 0
+*/
+
+int main(void)
+{
+	char i = 97;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+
+	return (0);
+}
