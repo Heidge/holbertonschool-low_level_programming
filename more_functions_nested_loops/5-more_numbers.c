cb9b5158368ca669b680dd84b64e39d001c4fe91
Heidge <axel.gore35@gmail.com>
2024-07-22 17:15:41 +0200

    Fix task 6 with remove condition that print newline

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,13 +5,17 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for(i = 0; i < 15; i++)
+	for (i = 0; i < 10; i++)
 	{
-		if (i > 9)
-			_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+
 }
