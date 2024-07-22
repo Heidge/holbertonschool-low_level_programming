94a0a9bc19c230a53d77ce2870b4d37145f563d5
Heidge <axel.gore35@gmail.com>
2024-07-22 17:16:58 +0200

    Fix task 6 with remove condition that print newline

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - function that prints the numbers from 0 to 9 except 2 & 4
+ * print_most_numbers - function that prints the numbers from 0 to 9 except:2, 4
  */
 void print_most_numbers(void)
 {
