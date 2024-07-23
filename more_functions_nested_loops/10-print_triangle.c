9f512333ddf0e7dacff49031c4252d49bacaafa5
Heidge <axel.gore35@gmail.com>
2024-07-23 09:32:59 +0200

    Fix fizz buzz and betty print_triangles

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,12 +11,12 @@ void print_triangle(int size)
 	if (size <= 0)
 		_putchar('\n');
 
-    for (i = 1; i <= size; i++)
-    {
-        for (j = size; j > i; j--)
-            _putchar(' ');
-        for (k = 1; k <= i; k++)
-            _putchar('#');
-        _putchar('\n');
-    }
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size; j > i; j--)
+			_putchar(' ');
+		for (k = 1; k <= i; k++)
+			_putchar('#');
+		_putchar('\n');
+	}
 }
