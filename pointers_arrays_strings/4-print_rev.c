59b593aaa166c5fa3710a1f9f0f55a73ee5d32a3
Heidge <axel.gore35@gmail.com>
2024-07-29 19:20:11 +0200

    Change task 4 but in don't remember why because i did tests after

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,12 +6,12 @@
  */
 void print_rev(char *s)
 {
-	int i = 1;
+	int i = 0;
 
-	while (s[i])
+	while (s[i] != '\0')
 		i++;
 
-	for (; i >= 0; i--)
+	for (i = i - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
