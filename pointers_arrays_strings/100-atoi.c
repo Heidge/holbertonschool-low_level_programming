cff1d59dedca63b79f487c659cd2580b50a302ab
Heidge <axel.gore35@gmail.com>
2024-09-10 16:11:36 +0200

    Change atoi function

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,10 +7,28 @@
  */
 int _atoi(char *s)
 {
-	int number = 0, i = 0;
+    int result = 0;
+    int sign = 1;
+    int i = 0;
 
-	for (; s[i] != '\0'; i++)
-        number = number * 10 + s[i] - '0';
+    // Check for leading sign character
 
-    return number;
+
+    // Convert digits to integer value
+    while (s[i] != '\0')
+	{
+        if (s[i] >= '0' && s[i] <= '9')
+		{
+			if (s[i - 1] == '-')
+				sign = -1;
+			result = (result * 10 + (s[i] - '0'));
+			i++;
+			if (s[i] < '0' || s[i] > '9')
+				return (sign * result);
+		}
+		else
+			i++;
+    }
+
+    return sign * result;
 }
