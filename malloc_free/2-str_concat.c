13f31c1f3086700eec717cecd6e839955a28c97e
Heidge <axel.gore35@gmail.com>
2024-11-06 16:10:31 +0100

    check if string privided are null

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -23,9 +23,11 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	}
 
-
-
 	str = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
+
+	if (str == NULL)
+		return NULL;
+		
 	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
 	for (j = 0; s2[j] != '\0'; j++, i++)
