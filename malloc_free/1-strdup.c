9055cb54d4c5f6a84bd588b12ddbcdea73686958
Heidge <axel.gore35@gmail.com>
2024-11-06 15:52:16 +0100

    Add strlen function and update min.h

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,8 +1,18 @@
 #include "main.h"
 
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+		length++;
+
+	return (length);
+}
+
 char *_strdup(char *str)
 {
-	char *str_copy = malloc(sizeof(char) * (strlen(str) + 1));
+	char *str_copy = malloc(sizeof(char) * (_strlen(str) + 1));
 	int i = 0;
 
 	if (str_copy == NULL || str == NULL)
--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,8 +1,18 @@
 #include "main.h"
 
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+		length++;
+
+	return (length);
+}
+
 char *str_concat(char *s1, char *s2)
 {
-	char *str = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+	char *str = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
 	int i, j = 0;
 
 	for (i = 0; s1[i] != '\0'; i++)
