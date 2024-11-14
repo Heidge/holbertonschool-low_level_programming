6161579720a866a2361adbcf91a2e929fed65251
Heidge <axel.gore35@gmail.com>
2024-11-14 20:29:03 +0100

    Push task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -25,17 +25,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (!string_concat)
 		return (NULL);
 
-	if (*s1)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-			string_concat[i] = s1[i];
-	}
 
-	if (*s2)
-	{
-		for (j = 0; j < n; j++, i++)
-			string_concat[i] = s2[j];
-	}
+	for (i = 0; s1[i] != '\0'; i++)
+		string_concat[i] = s1[i];
+
+	for (j = 0; j < n; j++, i++)
+		string_concat[i] = s2[j];
+
 
 	string_concat[i] = '\0';
 
