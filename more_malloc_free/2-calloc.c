a643d82c3ebaf53ca240e8b5498124e4af5f31e8
Axel Goré <105491625+Heidge@users.noreply.github.com>
2025-07-04 10:50:22 +0200

    Push task 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,5 +2,19 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *array;
+	int index = 0;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	array = malloc(size * nmemb);
+
+	while (array[index])
+	{
+		array[index] = 0;
+		index++;
+	}
+
+	return (array);
 }
