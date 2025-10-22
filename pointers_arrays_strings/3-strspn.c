04436a49ccaa2c2b82319bd689bd6c381fc8eee6
Axel Goré <105491625+Heidge@users.noreply.github.com>
2025-10-22 16:18:00 +0200

    test task 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that updates the value points by the parameter
+ * @n: The pointer
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+	int check = 0;
+
+	for (i = 0; i < _strlen(s); i++) {
+		for (j = 0; j < _strlen(accept); j++)
+		{
+			if (s[i] == accept[j])
+			{
+				check = 0;
+				break;
+			}
+			else
+				check = 1;
+		}
+		if (check == 1)
+			break;
+	}
+	return (i);
+}
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+		length++;
+
+	return (length);
+}
