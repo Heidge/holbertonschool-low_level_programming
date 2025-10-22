e981a8e73c0e2f3cab3687c27326e88b011b9144
Axel Goré <105491625+Heidge@users.noreply.github.com>
2025-10-22 15:49:34 +0200

    Test task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that updates the value points by the parameter
+ * @n: The pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; i < _strlen(s); i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	return (NULL);
+}
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+		length++;
+
+	return (length);
+}
