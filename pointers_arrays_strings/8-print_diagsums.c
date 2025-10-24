33f2369859213c45d2d86f30e22ad8ee2e1228c1
Axel Goré <105491625+Heidge@users.noreply.github.com>
2025-10-24 14:24:22 +0200

    Test task 8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -6,5 +6,21 @@
  */
 void print_diagsums(int *a, int size)
 {
-	
+	int i = 0;
+	int sum_1 = 0;
+	int sum_2 = 0;
+
+	for (i = 0; i < (size * size); i++)
+	{
+		if (i == (size * size - 1) / 2 )
+		{
+			sum_1 += a[i];
+			sum_2 += a[i];
+		}
+		else if (i % (size + 1) == 0)
+			sum_1 += a[i];
+		else if (i % (size - 1) == 0)
+			sum_2 += a[i];
+	}
+	printf("%d, %d\n", sum_1, sum_2);
 }
