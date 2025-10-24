6e597cb9daa51b6c93e847f8b03fdbac161e953f
Axel Goré <105491625+Heidge@users.noreply.github.com>
2025-10-24 11:18:00 +0200

    Test task 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -6,5 +6,31 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	
+	int i = 0;
+	int j = 0;
+	int len_haystack = _strlen(haystack);
+    int len_needle = _strlen(needle);
+
+	for (i = 0; i < len_haystack; i++)
+	{
+		for (j = 0; j < len_needle; j++)
+		{
+			if (haystack[i + j] == needle[j])
+			{
+				if (j == len_needle - 1)
+					return (haystack+i);
+			}
+		}
+	}
+	return (NULL);
+}
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+		length++;
+
+	return (length);
 }
