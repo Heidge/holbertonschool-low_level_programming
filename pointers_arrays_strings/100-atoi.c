8bda108d6d162e2dba5fbaf8dbcd32de91392743
Axel Goré <105491625+Heidge@users.noreply.github.com>
2026-02-12 16:57:50 +0100

    fix: atoi task

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,10 +13,14 @@ int _atoi(char *s)
 
     while (s[i] != '\0')
 	{
+
+
+		if (s[i] == '-')
+				sign *= -1;
+
+
         if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (s[i - 1] == '-')
-				sign = -1;
 			result = (result * 10 + (s[i] - '0'));
 			i++;
 			if (s[i] < '0' || s[i] > '9')
